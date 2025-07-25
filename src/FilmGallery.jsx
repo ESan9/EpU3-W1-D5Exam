abe3fb84ae319d1add7cc9b52a2743f62a31bb89
EmanSan9 <sanna950@gmail.com>
2025-07-25 14:14:47 +0200

    VFINAL+1EXTRA

--- src/FilmGallery.jsx
+++ src/FilmGallery.jsx
@@ -1,12 +1,15 @@
 import { Component } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import "../assets/FilmGallery.css";
+import { Spinner, Alert } from "react-bootstrap";
 
 const endpoint = "http://www.omdbapi.com/?apikey=78413715&s=";
 
 class FilmGallery extends Component {
   state = {
     films: [],
+    isLoading: true,
+    isError: false,
   };
 
   getFilms = () => {
@@ -17,12 +20,23 @@ class FilmGallery extends Component {
       })
       .then((data) => {
         if (data.Search) {
-          this.setState({ films: data.Search });
+          this.setState({
+            films: data.Search,
+            isLoading: false,
+            isError: false,
+          });
         } else {
-          this.setState({ films: [] });
+          this.setState({
+            films: [],
+            isLoading: false,
+            isError: false,
+          });
         }
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(err);
+        this.setState({ isError: true, isLoading: false });
+      });
   };
 
   componentDidMount() {
@@ -36,6 +50,17 @@ class FilmGallery extends Component {
         style={{ backgroundColor: "#141414", borderRadius: "6px" }}
       >
         <h5 className="text-white mb-3">{this.props.search}</h5>
+        {/* Spinner e gestione errore nel recuper film */}
+        {this.state.isLoading && (
+          <div className="text-center mb-3">
+            <Spinner animation="border" variant="success" />
+          </div>
+        )}
+        {this.state.isError && (
+          <Alert variant="danger" className="text-center">
+            Errore nel recupero film 😥
+          </Alert>
+        )}
         <Row>
           {this.state.films.slice(0, 6).map((film) => (
             <Col key={film.imdbID} xs={6} md={4} lg={2} className="mb-4">
@@ -47,7 +72,7 @@ class FilmGallery extends Component {
                     : "https://placecats.com/300/450"
                 }
                 alt={film.Title}
-                // Qui ti ho messo una semplice gestione errore per l'extra dato che per Cowboy Bebop non trovava una cover l'ho sostituita con placecats
+                // Qui ho messo una semplice gestione errore per l'extra dato che per Cowboy Bebop e Eva non trovava una cover. La sostituisce con un placecats.
                 onError={(e) => {
                   e.target.onerror = null;
                   e.target.src = "https://placecats.com/300/450";
